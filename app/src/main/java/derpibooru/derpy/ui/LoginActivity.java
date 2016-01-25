720b72d4e2165ef36553a4cbbbc6271e5ba931f3
deliciousblackink <deliciousblackink@gmail.com>
2016-01-25 07:38:53 +0700

    LoginActivity UI logic

--- app/src/main/java/derpibooru/derpy/ui/LoginActivity.java
+++ app/src/main/java/derpibooru/derpy/ui/LoginActivity.java
@@ -1,25 +1,62 @@
 package derpibooru.derpy.ui;
 
+import android.content.Context;
 import android.graphics.Typeface;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import derpibooru.derpy.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int MIN_ACCEPTED_PASSWORD_LENGTH = 6;
+
+    private EditText mEmailText;
+    private EditText mPasswordText;
+    private boolean mIsAuthInProgress = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mEmailText = ((EditText) findViewById(R.id.textEmail));
+        mPasswordText = ((EditText) findViewById(R.id.textPassword));
+
         ((TextInputLayout) findViewById(R.id.inputEmail))
                 .setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf"));
-
         ((TextInputLayout) findViewById(R.id.inputPassword))
                 .setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf"));
+
+        findViewById(R.id.buttonLogin).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    login();
+                }
+            });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean("authInProgress", mIsAuthInProgress);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mIsAuthInProgress = savedInstanceState.getBoolean("authInProgress");
+        if (mIsAuthInProgress) {
+            showProgressBar();
+        }
     }
 
     /* Respond to ActionBar's Up (Back) button */
@@ -31,5 +68,55 @@ public class LoginActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showSnackbar(String content) {
+        Snackbar.make(findViewById(android.R.id.content),
+                      content, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void login() {
+        String email = mEmailText.getText().toString();
+        String password = mPasswordText.getText().toString();
+        if (!isEmailValid(email)) {
+            showSnackbar("Invalid e-mail");
+            return;
+        }
+        if (password.length() < MIN_ACCEPTED_PASSWORD_LENGTH) {
+            showSnackbar("The password is too short");
+            return;
+        }
+        mIsAuthInProgress = true;
+        showProgressBar();
+    }
+
+    private void showProgressBar() {
+        ProgressBar progress = (ProgressBar) findViewById(R.id.progressLogin);
+        progress.getIndeterminateDrawable()
+                .setColorFilter(ContextCompat.getColor(this, R.color.colorAccent),
+                                android.graphics.PorterDuff.Mode.SRC_IN);
+        progress.setVisibility(View.VISIBLE);
+        progress.setFocusable(true);
+
+        mEmailText.setTag(mEmailText.getKeyListener());
+        mEmailText.setKeyListener(null);
+        mEmailText.setFocusable(false);
+        mPasswordText.setTag(mPasswordText.getKeyListener());
+        mPasswordText.setKeyListener(null);
+        mPasswordText.setFocusable(false);
+        findViewById(R.id.checkRememberMe).setEnabled(false);
+
+        InputMethodManager inputManager =
+                (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManager != null) {
+            inputManager.hideSoftInputFromWindow(mEmailText.getWindowToken(), 0);
+            inputManager.hideSoftInputFromWindow(mPasswordText.getWindowToken(), 0);
+        }
+
+        findViewById(R.id.buttonLogin).setVisibility(View.GONE);
+    }
+
+    private boolean isEmailValid(String email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }

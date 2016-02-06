8deb5b9cc2ef00adc945f6b9250a893aee900adf
deliciousblackink <deliciousblackink@gmail.com>
2016-02-06 23:17:58 +0700

    Image comments backend

---
+++ app/src/main/java/derpibooru/derpy/server/ImageCommentsProvider.java
@@ -0,0 +1,54 @@
+package derpibooru.derpy.server;
+
+import android.content.Context;
+
+import derpibooru.derpy.server.parsers.ImageCommentsParser;
+
+public class ImageCommentsProvider extends Provider {
+    private int mCurrentPage = 1;
+    private int mImageId;
+
+    public ImageCommentsProvider(Context context, ProviderRequestHandler handler) {
+        super(context, handler);
+    }
+
+    /**
+     * Returns an ImageCommentsProvider for the particular image ID.
+     *
+     * @param id image ID
+     */
+    public ImageCommentsProvider id(int id) {
+        mImageId = id;
+        return this;
+    }
+
+    public ImageCommentsProvider nextPage() {
+        mCurrentPage++;
+        return this;
+    }
+
+    protected int getCurrentPage() {
+        return mCurrentPage;
+    }
+
+    protected void resetPageNumber() {
+        mCurrentPage = 1;
+    }
+
+    @Override
+    protected String generateUrl() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(DERPIBOORU_DOMAIN);
+        sb.append("images/");
+        sb.append(mImageId);
+        sb.append("/comments");
+        sb.append("?page=");
+        sb.append(getCurrentPage());
+        return sb.toString();
+    }
+
+    @Override
+    public void fetch() {
+        super.executeQuery(generateUrl(), new ImageCommentsParser());
+    }
+}

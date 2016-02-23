b04bc728536942b7c1a9db33d7a9370a3616d9c3
deliciousblackink <deliciousblackink@gmail.com>
2016-02-23 10:08:50 +0700

    Code reformat

--- app/src/main/java/derpibooru/derpy/ui/fragments/MainActivityRankingsTabFragment.java
+++ app/src/main/java/derpibooru/derpy/ui/fragments/MainActivityRankingsTabFragment.java
@@ -5,21 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import derpibooru.derpy.server.providers.RankingsProvider;
+import derpibooru.derpy.server.providers.RankingImageListProvider;
 
 public class MainActivityRankingsTabFragment extends ImageListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        super.setImageListProvider(new RankingsProvider(getActivity(),
+        super.setImageListProvider(new RankingImageListProvider(getActivity(),
                                                         new ImageListRequestHandler()));
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
     protected void fetchImages() {
-        ((RankingsProvider) super.getImageListProvider())
-                .type(RankingsProvider.RankingsType.fromValue(getArguments().getInt("type")))
+        ((RankingImageListProvider) super.getImageListProvider())
+                .type(RankingImageListProvider.RankingsType.fromValue(getArguments().getInt("type")))
                 .inDays(3) /* TODO: pass the time limit as an argument */
                 .fetch();
     }

a3e2945b431b9674f86777bed2dd5e0f24695a77
deliciousblackink <deliciousblackink@gmail.com>
2016-02-23 10:21:24 +0700

    Restructured navigation drawer

--- app/src/main/java/derpibooru/derpy/ui/NavigationDrawer.java
+++ app/src/main/java/derpibooru/derpy/ui/NavigationDrawer.java
@@ -3,108 +3,115 @@ package derpibooru.derpy.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
 
 import derpibooru.derpy.R;
 import derpibooru.derpy.data.server.DerpibooruUser;
 import derpibooru.derpy.server.QueryHandler;
 import derpibooru.derpy.server.providers.UserDataProvider;
+import derpibooru.derpy.server.providers.UserImageListProvider;
 import derpibooru.derpy.server.requesters.LogoutRequester;
 
-class NavigationDrawer implements NavigationView.OnNavigationItemSelectedListener {
+class NavigationDrawer extends NavigationDrawerLayout {
     private static final int LOGIN_ACTIVITY_REQUEST_CODE = 1;
-
-    private NavigationDrawerActivity mParent;
-    private int mParentNavigationId;
-
-    private NavigationView mNavigationView;
-    private DrawerLayout mDrawerLayout;
-    private View mDrawerHeader;
 
     private UserDataProvider mUserProvider;
 
     NavigationDrawer(NavigationDrawerActivity parent, DrawerLayout drawer, Toolbar toolbar, NavigationView menu) {
-        mParent = parent;
+        super(parent, drawer, toolbar, menu);
         setActivityMenuItemId();
-
-        mDrawerLayout = drawer;
-        mNavigationView = ((NavigationView) mDrawerLayout.findViewById(R.id.navigationView));
-        mDrawerHeader = mNavigationView.getHeaderView(0);
-
-        mDrawerHeader.findViewById(R.id.buttonRefreshUserData)
-                .setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        refreshUserData();
-                    }
-                });
-
-        initDrawerToggle(parent, toolbar, drawer, menu);
 
         mUserProvider = new UserDataProvider(parent, new UserQueryHandler());
         mUserProvider.fetch();
     }
 
-    private void setActivityMenuItemId() {
-        if (mParent instanceof MainActivity) {
-            mParentNavigationId = R.id.navigationHome;
-        } else if (mParent instanceof SearchActivity) {
-            mParentNavigationId = R.id.navigationSearch;
-        } else if (mParent instanceof FiltersActivity) {
-            mParentNavigationId = R.id.navigationFilters;
-        }
-    }
-
-    public void refreshUserData() {
-        mDrawerHeader.findViewById(R.id.buttonRefreshUserData)
-                .setVisibility(View.INVISIBLE);
-        ((TextView) mDrawerHeader.findViewById(R.id.textHeaderFilter))
-                .setText("Loading...");
+    @Override
+    protected void fetchUserData() {
         mUserProvider.refreshUserData();
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == mParentNavigationId) {
+        return isParentActivitySelected(item.getItemId())
+                || isAnotherActivitySelected(item.getItemId())
+                || isUserImageListActivitySelected(item.getItemId())
+                || isAuthenticationActionSelected(item.getItemId());
+    }
+
+    private boolean isParentActivitySelected(int itemId) {
+        if (itemId == mParentNavigationId) {
             closeDrawer();
             return true;
         }
-        switch (id) {
-            case (R.id.navigationHome):
-                mParent.startActivity(new Intent(mParent, MainActivity.class));
-                mParent.finish();
-                break;
-            case (R.id.navigationSearch):
-                mParent.startActivity(new Intent(mParent, SearchActivity.class));
-                mParent.finish();
-                break;
-            case (R.id.navigationFilters):
-                mParent.startActivity(new Intent(mParent, FiltersActivity.class));
-                mParent.finish();
-                break;
-            case (R.id.navigationLogin):
-                mParent.startActivityForResult(new Intent(mParent, LoginActivity.class),
-                                               LOGIN_ACTIVITY_REQUEST_CODE);
-                mNavigationView.getMenu()
-                        .findItem(mParentNavigationId).setChecked(false);
-                break;
-            case (R.id.navigationLogout):
-                logout();
-                return true; /* do not hide the drawer */
+        return false;
+    }
+
+    private boolean isAnotherActivitySelected(int itemId) {
+        for (Map.Entry<Integer, Class<?>> activityItem : ACTIVITY_NAVIGATION_ACTIONS.entrySet()) {
+            if (activityItem.getKey() == itemId) {
+                deselectParentMenuItemAndCloseDrawer();
+                mParent.startActivity(new Intent(mParent, activityItem.getValue()));
+                return true;
+            }
         }
-        closeDrawer();
+        return false;
+    }
+
+    private boolean isUserImageListActivitySelected(int itemId) {
+        Intent intent = new Intent(mParent, UserImageListActivity.class);
+        switch (itemId) {
+            case R.id.navigationFaves:
+                intent.putExtra("type", UserImageListProvider.UserListType.Faved.toValue());
+                break;
+            case R.id.navigationUpvoted:
+                intent.putExtra("type", UserImageListProvider.UserListType.Upvoted.toValue());
+                break;
+            case R.id.navigationUploaded:
+                intent.putExtra("type", UserImageListProvider.UserListType.Uploaded.toValue());
+                break;
+            default:
+                return false;
+        }
+        deselectParentMenuItemAndCloseDrawer();
+        mParent.startActivity(intent);
         return true;
+    }
+
+    private boolean isAuthenticationActionSelected(int itemId) {
+        if (itemId == R.id.navigationLogin) {
+            mParent.startActivityForResult(
+                    new Intent(mParent, LoginActivity.class), LOGIN_ACTIVITY_REQUEST_CODE);
+            deselectParentMenuItemAndCloseDrawer();
+            return true;
+        } else if (itemId == R.id.navigationLogout) {
+            logout();
+            return true; /* don't close the drawer */
+        }
+        return false;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LOGIN_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                mUserProvider.refreshUserData();
+            }
+            mNavigationView.getMenu().findItem(R.id.navigationLogin).setChecked(false);
+            mNavigationView.getMenu().findItem(mParentNavigationId).setChecked(false);
+        }
+    }
+
+    private void setActivityMenuItemId() {
+        for (Map.Entry<Integer, Class<?>> activityItem : ACTIVITY_NAVIGATION_ACTIONS.entrySet()) {
+            if (activityItem.getValue().equals(mParent.getClass())) {
+                mParentNavigationId = activityItem.getKey();
+            }
+        }
     }
 
     private void logout() {
@@ -119,83 +126,6 @@ class NavigationDrawer implements NavigationView.OnNavigationItemSelectedListene
         }).fetch();
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case (LOGIN_ACTIVITY_REQUEST_CODE):
-                if (resultCode == Activity.RESULT_OK) {
-                    mUserProvider.refreshUserData();
-                }
-                mNavigationView.getMenu().findItem(R.id.navigationLogin).setChecked(false);
-
-                mNavigationView.getMenu()
-                        .findItem(mParentNavigationId).setChecked(false);
-                break;
-        }
-    }
-
-    private void displayUserData(DerpibooruUser user) {
-        mDrawerHeader.findViewById(R.id.buttonRefreshUserData)
-                .setVisibility(View.VISIBLE);
-        if (!user.isLoggedIn()) {
-            onUserLoggedOut(user);
-        } else {
-            onUserLoggedIn(user);
-        }
-        ((TextView) mDrawerHeader.findViewById(R.id.textHeaderFilter))
-                .setText("Filter: " + user.getCurrentFilter().getName());
-        /* ! copied from ImageCommentsAdapter; perhaps it should be made into a separate class? */
-        if (!user.getAvatarUrl().endsWith(".svg")) {
-            Glide.with(mParent).load(user.getAvatarUrl()).diskCacheStrategy(DiskCacheStrategy.RESULT)
-                    .dontAnimate().into((ImageView) mDrawerHeader.findViewById(R.id.imageAvatar));
-        } else {
-            Glide.with(mParent).load(R.drawable.no_avatar).dontAnimate()
-                    .into((ImageView) mDrawerHeader.findViewById(R.id.imageAvatar));
-        }
-    }
-
-    private void onUserLoggedIn(DerpibooruUser user) {
-        ((TextView) mDrawerHeader.findViewById(R.id.textHeaderUser))
-                .setText(user.getUsername());
-        mNavigationView.getMenu().clear();
-        mNavigationView.inflateMenu(R.menu.menu_navigation_drawer_logged_in);
-        mNavigationView.getMenu()
-                .findItem(mParentNavigationId).setChecked(true);
-    }
-
-    private void onUserLoggedOut(DerpibooruUser user) {
-        ((TextView) mDrawerHeader.findViewById(R.id.textHeaderUser))
-                .setText("Not logged in");
-        mNavigationView.getMenu().clear();
-        mNavigationView.inflateMenu(R.menu.menu_navigation_drawer_logged_out);
-        mNavigationView.getMenu()
-                .findItem(mParentNavigationId).setChecked(true);
-    }
-
-    private void initDrawerToggle(Activity parent, Toolbar toolbar,
-                                  DrawerLayout drawer, NavigationView menu) {
-        ActionBarDrawerToggle toggle =
-                new ActionBarDrawerToggle(parent, mDrawerLayout, toolbar,
-                                          R.string.open_drawer, R.string.close_drawer) {
-                    @Override
-                    public void onDrawerSlide(View drawerView, float slideOffset) {
-                        /* Disable the hamburger icon animation (for more info refer to
-                         * https://medium.com/android-news/navigation-drawer-styling-according-material-design-5306190da08f#.9wrzhczd8 ) */
-                        super.onDrawerSlide(drawerView, 0);
-                    }
-                };
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-        menu.setNavigationItemSelectedListener(this);
-    }
-
-    public boolean isDrawerOpen() {
-        return mDrawerLayout.isDrawerOpen(GravityCompat.START);
-    }
-
-    public void closeDrawer() {
-        mDrawerLayout.closeDrawer(GravityCompat.START);
-    }
-
     private class UserQueryHandler implements QueryHandler<DerpibooruUser> {
         @Override
         public void onQueryExecuted(DerpibooruUser result) {
@@ -206,4 +136,10 @@ class NavigationDrawer implements NavigationView.OnNavigationItemSelectedListene
         @Override
         public void onQueryFailed() { }
     }
+
+    private static final Map<Integer, Class<?>> ACTIVITY_NAVIGATION_ACTIONS =
+            ImmutableMap.<Integer, Class<?>>builder()
+            .put(R.id.navigationHome, MainActivity.class)
+            .put(R.id.navigationSearch, SearchActivity.class)
+            .put(R.id.navigationFilters, FiltersActivity.class).build();
 }
---
+++ app/src/main/java/derpibooru/derpy/ui/NavigationDrawerLayout.java
@@ -0,0 +1,126 @@
+package derpibooru.derpy.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.common.collect.ImmutableMap;
+
+import derpibooru.derpy.R;
+import derpibooru.derpy.data.server.DerpibooruUser;
+
+abstract class NavigationDrawerLayout implements NavigationView.OnNavigationItemSelectedListener {
+    protected NavigationDrawerActivity mParent;
+    protected int mParentNavigationId;
+
+    protected NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
+    private View mDrawerHeader;
+
+    NavigationDrawerLayout(NavigationDrawerActivity parent, DrawerLayout drawer, Toolbar toolbar, NavigationView menu) {
+        mParent = parent;
+
+        mDrawerLayout = drawer;
+        mNavigationView = ((NavigationView) mDrawerLayout.findViewById(R.id.navigationView));
+        mDrawerHeader = mNavigationView.getHeaderView(0);
+
+        mDrawerHeader.findViewById(R.id.buttonRefreshUserData)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        refreshUserData();
+                    }
+                });
+
+        initDrawerToggle(parent, toolbar, drawer, menu);
+    }
+
+    protected abstract void fetchUserData();
+
+    public void refreshUserData() {
+        mDrawerHeader.findViewById(R.id.buttonRefreshUserData)
+                .setVisibility(View.INVISIBLE);
+        ((TextView) mDrawerHeader.findViewById(R.id.textHeaderFilter))
+                .setText(R.string.loading);
+        fetchUserData();
+    }
+
+    protected void displayUserData(DerpibooruUser user) {
+        mDrawerHeader.findViewById(R.id.buttonRefreshUserData)
+                .setVisibility(View.VISIBLE);
+        if (!user.isLoggedIn()) {
+            onUserLoggedOut(user);
+        } else {
+            onUserLoggedIn(user);
+        }
+        ((TextView) mDrawerHeader.findViewById(R.id.textHeaderFilter))
+                .setText(String.format(mParent.getString(R.string.user_filter),
+                                       user.getCurrentFilter().getName()));
+        /* ! copied from ImageCommentsAdapter; perhaps it should be made into a separate class? */
+        if (!user.getAvatarUrl().endsWith(".svg")) {
+            Glide.with(mParent).load(user.getAvatarUrl()).diskCacheStrategy(DiskCacheStrategy.RESULT)
+                    .dontAnimate().into((ImageView) mDrawerHeader.findViewById(R.id.imageAvatar));
+        } else {
+            Glide.with(mParent).load(R.drawable.no_avatar).dontAnimate()
+                    .into((ImageView) mDrawerHeader.findViewById(R.id.imageAvatar));
+        }
+    }
+
+    private void onUserLoggedIn(DerpibooruUser user) {
+        ((TextView) mDrawerHeader.findViewById(R.id.textHeaderUser))
+                .setText(user.getUsername());
+        mNavigationView.getMenu().clear();
+        mNavigationView.inflateMenu(R.menu.menu_navigation_drawer_logged_in);
+        mNavigationView.getMenu()
+                .findItem(mParentNavigationId).setChecked(true);
+    }
+
+    private void onUserLoggedOut(DerpibooruUser user) {
+        ((TextView) mDrawerHeader.findViewById(R.id.textHeaderUser))
+                .setText(R.string.user_logged_out);
+        mNavigationView.getMenu().clear();
+        mNavigationView.inflateMenu(R.menu.menu_navigation_drawer_logged_out);
+        mNavigationView.getMenu()
+                .findItem(mParentNavigationId).setChecked(true);
+    }
+
+    private void initDrawerToggle(Activity parent, Toolbar toolbar,
+                                  DrawerLayout drawer, NavigationView menu) {
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(parent, mDrawerLayout, toolbar,
+                                          R.string.open_drawer, R.string.close_drawer) {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        /* Disable the hamburger icon animation (for more info refer to
+                         * https://medium.com/android-news/navigation-drawer-styling-according-material-design-5306190da08f#.9wrzhczd8 ) */
+                        super.onDrawerSlide(drawerView, 0);
+                    }
+                };
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        menu.setNavigationItemSelectedListener(this);
+    }
+
+    public boolean isDrawerOpen() {
+        return mDrawerLayout.isDrawerOpen(GravityCompat.START);
+    }
+
+    public void closeDrawer() {
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    protected void deselectParentMenuItemAndCloseDrawer() {
+        mNavigationView.getMenu().findItem(mParentNavigationId).setChecked(false);
+        closeDrawer();
+    }
+}

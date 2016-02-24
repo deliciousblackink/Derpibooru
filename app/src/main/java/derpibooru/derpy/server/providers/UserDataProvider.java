afc792c2b5c0d3a4a73347876f53b00d2ddb7d97
deliciousblackink <deliciousblackink@gmail.com>
2016-02-24 17:31:14 +0700

    Removed obsolete classes

--- app/src/main/java/derpibooru/derpy/server/providers/UserDataProvider.java
+++ app/src/main/java/derpibooru/derpy/server/providers/UserDataProvider.java
@@ -5,24 +5,14 @@ import android.content.Context;
 import derpibooru.derpy.data.server.DerpibooruUser;
 import derpibooru.derpy.server.QueryHandler;
 import derpibooru.derpy.server.parsers.UserDataParser;
-import derpibooru.derpy.storage.UserDataStorage;
 
 public class UserDataProvider extends Provider<DerpibooruUser> {
-    private UserDataStorage mUserDataStorage;
-
     public UserDataProvider(Context context, QueryHandler<DerpibooruUser> handler) {
         super(context, handler);
-        mUserDataStorage = new UserDataStorage(context);
     }
 
     public void refreshUserData() {
-        mUserDataStorage.clearUserData();
         fetch();
-    }
-
-    public boolean isLoggedIn() {
-        return mUserDataStorage.getUserData() != null
-                && mUserDataStorage.getUserData().isLoggedIn();
     }
 
     @Override
@@ -36,22 +26,6 @@ public class UserDataProvider extends Provider<DerpibooruUser> {
 
     @Override
     public void fetch() {
-        if (!isUserDataCached()) {
-            super.executeQuery(new UserDataParser());
-        }
-    }
-
-    @Override
-    protected void cacheResponse(DerpibooruUser parsedResponse) {
-        mUserDataStorage.setUserData(parsedResponse);
-    }
-
-    private boolean isUserDataCached() {
-        DerpibooruUser cached = mUserDataStorage.getUserData();
-        if (cached != null) {
-            mHandler.onQueryExecuted(cached);
-            return true;
-        }
-        return false;
+        super.executeQuery(new UserDataParser());
     }
 }

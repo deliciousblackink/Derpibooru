aff8ec23a9a32b7a825d09254a457a052d024746
deliciousblackink <deliciousblackink@gmail.com>
2016-04-12 20:28:36 +0700

    Remove placeholder thumb from adapter

--- app/src/main/java/derpibooru/derpy/ui/adapters/ImageActivityFragmentAdapter.java
+++ app/src/main/java/derpibooru/derpy/ui/adapters/ImageActivityFragmentAdapter.java
@@ -1,41 +1,43 @@
 package derpibooru.derpy.ui.adapters;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
 import derpibooru.derpy.R;
-import derpibooru.derpy.data.server.DerpibooruImageDetailed;
-import derpibooru.derpy.data.server.DerpibooruImageThumb;
-import derpibooru.derpy.data.server.DerpibooruUser;
-import derpibooru.derpy.ui.ImageActivity;
-import derpibooru.derpy.ui.MainActivity;
 import derpibooru.derpy.ui.fragments.ImageActivityMainFragment;
 import derpibooru.derpy.ui.fragments.ImageActivityTagFragment;
-import derpibooru.derpy.ui.fragments.ImageListFragment;
 
 public abstract class ImageActivityFragmentAdapter extends FragmentAdapter {
-    public ImageActivityFragmentAdapter(FragmentManager fragmentManager,
-                                        int fragmentLayoutId,
-                                        @Nullable Bundle savedInstanceState,
-                                        @Nullable DerpibooruImageThumb placeholderThumb) {
+    public ImageActivityFragmentAdapter(FragmentManager fragmentManager, int fragmentLayoutId) {
         super(fragmentManager, fragmentLayoutId);
-        setFragmentCallbackHandlers(getCurrentFragment());
-        if ((savedInstanceState != null) && (savedInstanceState.containsKey(ImageActivity.EXTRAS_IMAGE_DETAILED))) {
-            if (getCurrentFragment() instanceof ImageActivityMainFragment) {
-                forceMainFragmentToDisplayDetailedImage(getCurrentFragment());
+        boolean isMainFragmentInstantiated = false;
+        if (fragmentManager.getFragments() != null) {
+            for (Fragment fragment : fragmentManager.getFragments()) {
+                setFragmentCallbackHandlers(fragment);
+                if (fragment instanceof ImageActivityMainFragment) {
+                    isMainFragmentInstantiated = true;
+                }
             }
-        } else if (placeholderThumb != null) {
-            displayMainFragmentWithPlaceholderThumb(placeholderThumb);
+        }
+        if (!isMainFragmentInstantiated) {
+            instantiateMainFragment();
+            fragmentManager.executePendingTransactions();
         }
     }
 
     protected abstract boolean isUserLoggedIn();
 
-    public void onDetailedImageFetched() {
-        displayMainFragmentWithDetailed();
+    public void displayMainFragmentWithToolbar(int imageId) {
+        if (getCurrentFragment() instanceof ImageActivityMainFragment) {
+            ((ImageActivityMainFragment) getCurrentFragment()).setImageId(imageId);
+        }
+    }
+
+    public void onImageDetailedFetched() {
+        if (getCurrentFragment() instanceof ImageActivityMainFragment) {
+            ((ImageActivityMainFragment) getCurrentFragment()).onDetailedImageFetched();
+        }
     }
 
     public void displayTagFragment(int tagId) {
@@ -53,49 +55,16 @@ public abstract class ImageActivityFragmentAdapter extends FragmentAdapter {
                 R.anim.image_activity_tag_back_stack_pop_exit);
     }
 
-    /**
-     * If {@link ImageActivityMainFragment} has not created its view yet, it is forced to
-     * skip the placeholder thumb and call {@link ImageActivityMainFragment.ImageActivityMainFragmentHandler#getImage()}.
-     */
-    private void forceMainFragmentToDisplayDetailedImage(Fragment mainFragment) {
-        mainFragment.getArguments().remove(ImageListFragment.EXTRAS_IMAGE_THUMB);
-    }
-
-    private void displayMainFragmentWithPlaceholderThumb(DerpibooruImageThumb placeholder) {
-        instantiateMainFragment(placeholder);
-    }
-
-    private void displayMainFragmentWithDetailed() {
-        if (getCurrentFragment() instanceof ImageActivityMainFragment) {
-            /* the main fragment has already been instantiated with a placeholder thumb */
-            ((ImageActivityMainFragment) getCurrentFragment())
-                    .onDetailedImageFetched();
-        } else {
-            instantiateMainFragment(null);
-        }
-    }
-
-    private Bundle getMainFragmentArguments(@Nullable DerpibooruImageThumb placeholder) {
-        Bundle args = new Bundle();
-        args.putBoolean(ImageActivityMainFragment.EXTRAS_IS_USER_LOGGED_IN, isUserLoggedIn());
-        if (placeholder != null) {
-            args.putParcelable(ImageListFragment.EXTRAS_IMAGE_THUMB, placeholder);
-        }
-        return args;
-    }
-
-    /*public void initializeMainFragment() {
-        if (getCurrentFragment() instanceof ImageActivityMainFragment) {
-            setFragmentCallbackHandlers(getCurrentFragment());
-            forceMainFragmentToDisplayDetailedImage(getCurrentFragment());
-        }
-    }*/
-
-    private void instantiateMainFragment(DerpibooruImageThumb placeholderThumb) {
+    private void instantiateMainFragment() {
         ImageActivityMainFragment mainFragment = new ImageActivityMainFragment();
-        mainFragment.setArguments(getMainFragmentArguments(placeholderThumb));
-
+        mainFragment.setArguments(getMainFragmentArguments());
         setFragmentCallbackHandlers(mainFragment);
         display(mainFragment);
+    }
+
+    private Bundle getMainFragmentArguments() {
+        Bundle args = new Bundle();
+        args.putBoolean(ImageActivityMainFragment.EXTRAS_IS_USER_LOGGED_IN, isUserLoggedIn());
+        return args;
     }
 }
